54a4ea49bc1b3a99bc45d03ed3ffde9ebbadd14d
Omama Arshad <omamaarshad1@gmail.com>
2026-04-30 13:44:02 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'https://project-and-team-management-app-xcfe-75dul7neu.vercel.app/')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:4200')],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
 
     'allowed_headers' => ['*'],
 
